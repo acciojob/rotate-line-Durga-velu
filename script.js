8452cccefb3091b8a8d55b18c4ae50c7c420d8d9
Durga-velu <durgadevi134babu@gmail.com>
2025-03-31 15:10:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 //your JS code here. If required.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let angle = 0;
+
+setInterval(() => {
+    angle += 5; 
+    document.getElementById("line").style.transform = `rotate(${angle}deg)`
+}, 20);
+
+
+
+
+
+
